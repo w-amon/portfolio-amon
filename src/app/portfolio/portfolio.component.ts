18b37325d57950ba2fb77e495524dbec00f140c0
Amon W <amon@Amons-MacBook-Air.local>
2023-11-06 09:59:32 +0300

    Add files

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Project } from '../_models/project';
 
 @Component({
   selector: 'app-portfolio',
@@ -7,6 +8,16 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent {
+  
+  project: Project = {
+    id: 0,
+    name: 'Sample Angular App',
+    summary: 'Test Description',
+    description: '',
+    projectLink: '',
+    pictures: [],
+    tags: ['Angular', 'TypeScript']
+  };
 
   constructor(private titleService: Title) {
     this.titleService.setTitle('Amon .W - Portfolio');
